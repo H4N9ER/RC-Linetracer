2521a0f2db85afdb4089fa12eae6b7fadaceb67d
H4N9ER <ehdgk0900@gmail.com>
2024-12-11 01:32:40 +0900

    motor.py updated with pigpio

--- motor.py
+++ motor.py
@@ -1,47 +1,47 @@
-import RPi.GPIO as GPIO
+import pigpio
 import time
 import cv2
 from picamera2 import Picamera2, Preview
 from libcamera import Transform
 import numpy as np
 
-Hertz = 100
-PWMA = 12
-NMA = 11
-PWMB = 35
-NMB = 36
+PWMA = 18
+NMA = 17
+PWMB = 19
+NMB = 16
 
 def motor_go(speed):
-    L_Motor.ChangeDutyCycle(speed)
-    R_Motor.ChangeDutyCycle(speed)
+    pi.set_PWM_dutycycle(PWMA, speed)
+    pi.set_PWM_dutycycle(PWMB, speed)
     
 def motor_right(speed):
-    L_Motor.ChangeDutyCycle(speed)
-    R_Motor.ChangeDutyCycle(0)
+    pi.set_PWM_dutycycle(PWMA, speed)
+    pi.set_PWM_dutycycle(PWMB, 0)
     
 def motor_left(speed):
-    L_Motor.ChangeDutyCycle(0)
-    R_Motor.ChangeDutyCycle(speed)
+    pi.set_PWM_dutycycle(PWMA, 0)
+    pi.set_PWM_dutycycle(PWMB, speed)
 
 def motor_stop():
-    L_Motor.ChangeDutyCycle(0)
-    R_Motor.ChangeDutyCycle(0)
+    pi.set_PWM_dutycycle(PWMA, 0)
+    pi.set_PWM_dutycycle(PWMB, 0)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+pi = pigpio.pi()
 
-GPIO.setup(PWMA, GPIO.OUT)
-GPIO.setup(NMA, GPIO.OUT)
-GPIO.setup(PWMB, GPIO.OUT)
-GPIO.setup(NMB, GPIO.OUT)
+pi.set_mode(PWMA, pigpio.ALT5)
+pi.set_mode(NMA, pigpio.OUTPUT)
+pi.set_mode(PWMB, pigpio.ALT0)
+pi.set_mode(NMB, pigpio.OUTPUT)
 
-L_Motor = GPIO.PWM(PWMA, Hertz)
-L_Motor.start(0)
-GPIO.output(NMA, False)
+pi.set_PWM_frequency(PWMA, 100)
+pi.set_PWM_range(PWMA, 100)
+pi.set_PWM_frequency(PWMB, 100)
+pi.set_PWM_range(PWMB, 100)
 
-R_Motor = GPIO.PWM(PWMB, Hertz)
-R_Motor.start(0)
-GPIO.output(NMB, False)
+pi.set_PWM_dutycycle(PWMA, 0)
+pi.set_PWM_dutycycle(PWMB, 0)
+pi.write(NMA, 0)
+pi.write(NMB, 0)
 
 
 
@@ -52,47 +52,50 @@ def main():
     picam2.start_preview(Preview.QTGL)
     picam2.start()
 
-    while True:
-        frame = picam2.capture_array("main")
-        cv2.imshow('normal',frame)
-        
-        crop_img =frame[60:120, 0:160]
-        
-        gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    
-        blur = cv2.GaussianBlur(gray,(5,5),0)
-        
-        ret,thresh1 = cv2.threshold(blur,130,255,cv2.THRESH_BINARY)
-        
-        mask = cv2.erode(thresh1, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        cv2.imshow('mask',mask)
-    
-        contours,hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
-        
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
-            M = cv2.moments(c)
+    try:
+        while True:
+            frame = picam2.capture_array("main")
+            cv2.imshow('normal',frame)
             
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
+            crop_img =frame[60:120, 0:160]
             
-            if cx >= 95 and cx <= 125:              
-                print("Turn Left!")
-                motor_left(50)
-            elif cx >= 39 and cx <= 65:
-                print("Turn Right")
-                motor_right(50)
-            else:
-                print("go")
-                motor_go(50)
+            gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
         
-            if cv2.waitKey(1) == ord('q'):
-                break
-    
+            blur = cv2.GaussianBlur(gray,(5,5),0)
+            
+            ret,thresh1 = cv2.threshold(blur,130,255,cv2.THRESH_BINARY)
+            
+            mask = cv2.erode(thresh1, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            cv2.imshow('mask',mask)
+        
+            contours,hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
+            
+            if len(contours) > 0:
+                c = max(contours, key=cv2.contourArea)
+                M = cv2.moments(c)
+                
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                
+                if cx >= 95 and cx <= 125:              
+                    print("Turn Left!")
+                    motor_left(50)
+                elif cx >= 39 and cx <= 65:
+                    print("Turn Right")
+                    motor_right(50)
+                else:
+                    print("go")
+                    motor_go(50)
+            
+                if cv2.waitKey(1) == ord('q'):
+                    break
+    except KeyboardInterrupt:
+        pass
+
     picam2.stop()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
-    GPIO.cleanup()
+    pi.stop()
